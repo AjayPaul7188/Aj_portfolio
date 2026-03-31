40976cf6f613fb9767d078c5bc12d3101d72b18d
AjayPaul7188 <ajaypaulmummdi@gmail.com>
2026-03-31 20:06:44 +0530

    Completed projects section

--- client/src/components/Projects/index.js
+++ client/src/components/Projects/index.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import './index.css'
 
 const projects = [
-  { title: 'Jobby App', github: 'https://github.com/AjayPaul7188/Jobby-App.git' },
-  { title: 'Emoji Game', github: 'https://github.com/AjayPaul7188/Emoji-Game.git' },
-  { title: 'Nxt Trends', github: 'https://github.com/AjayPaul7188/nxtTrends.git' }
+  { title: 'Jobby App', liveDemo: 'https://ajjobapp.ccbp.tech/login', github: 'https://github.com/AjayPaul7188/Jobby-App.git', img: '/images/JobImage.jpg', intro: 'Implemented Jobby App where users can log in and can see a list of jobs with search by Job title, filters based on Salary range and Employment type, etc' },
+  { title: 'Emoji Game', liveDemo: 'https://emojigameaj.ccbp.tech/', github: 'https://github.com/AjayPaul7188/Emoji-Game.git', img: '/images/Emojiimage.jpg', intro: 'Bulit an Emoji Game where users can select unique emojies and get score card' },
+  { title: 'Nxt Trends', liveDemo: 'https://ajtrnds.ccbp.tech/login', github: 'https://github.com/AjayPaul7188/nxtTrends.git', img: '/images/TrendsImage.jpg', intro: 'Created a modern e-commerce experience with a design inspired by Amazon and Flipkart.' }
 ];
 
 export default function Projects() {
   return (
     <section className="section">
-      <div className="glass">
-        <h2>Projects</h2>
-        {projects.map((p, i) => (
-          <motion.div key={i} whileHover={{ scale: 1.05 }}>
-            <h3>{p.title}</h3>
-            <a href={p.github}>GitHub</a>
-          </motion.div>
-        ))}
+      <div className="glass project-section" >
+        <h1 style={{color: '#8b5cf6'}}>Projects</h1>
+        <div className='projects'>
+            {projects.map((p, i) => (
+            <motion.div className='project-tab' key={i} whileHover={{ scale: 1.05 }}>
+                <img className='project-img' src={p.img} alt='Project pics' />
+                <div>
+                    <h3 style={{color: '#a78bfa', marginBottom: '0px'}}>{p.title}</h3>
+                    <p className='para' style={{fontSize: '12px'}}>{p.intro}</p>
+                </div>
+                <div style={{display: 'flex', alignItems: 'center'}}>
+                    <button className='btn'><a href={p.github} style={{textDecoration: 'none', color: '#000000', fontWeight: '600'}} target='_blank' rel='noopener norefferer' >GitHub</a></button>
+                    <button className='btn'><a href={p.liveDemo} style={{textDecoration: 'none', color: '#000000', fontWeight: '600'}} target='_blank' rel='noopener norefferer' >Live Demo</a></button>
+                </div>
+            </motion.div>
+            ))}
+        </div>
       </div>
     </section>
   );
